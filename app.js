6d55b8ad04209010bc78ca5ae0fd9bea9e526a53
Ivan <62508347+ivanEarl@users.noreply.github.com>
2025-02-19 13:49:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const app = express();
 const port = 3000;
 const routes = require('./router/routes');
 
-// Custom logging middleware
 const logRequests = (req, res, next) => {
     console.log(`${req.method} ${req.url}`);
-    next(); // Pass control to the next middleware function
+    next();
 };
 
 app.use(logRequests);
-app.use(express.json()); // Parse JSON request bodies middleware
+app.use(express.json());
 
-app.use('/', routes); //routes
+app.use('/', routes);
 
 app.listen(port, () => {
     console.log("Server started: Running at port ", port)
